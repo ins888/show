580587ea940950b70f150582064751d159e9d532
ins888 <73679447+ins888@users.noreply.github.com>
2020-11-19 19:56:42 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -13,7 +13,7 @@
     <?php
     $id=$_GET['ID'];
     $conn = mysqli_init();
-    mysqli_real_connect($conn, 'ttt.mysql.database.azure.com', 'ins888@ttt', '022943654Pp', 'itflab', 3306);
+    mysqli_real_connect($conn, 'ttt.mysql.database.azure.com', 'ins888@ttt', '022943654Pp', 'itflaab', 3306);
     $res = mysqli_query($conn, "SELECT * FROM guestbook WHERE ID='$id'");
     $row = mysqli_fetch_array($res)
     ?>
