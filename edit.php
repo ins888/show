d80b27c7d069e4013c6a78b205d770b533832555
ins888 <73679447+ins888@users.noreply.github.com>
2020-11-19 20:44:23 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -21,11 +21,11 @@
     <h2>Edit Form</h2>
     <form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm">
         <label for="Username">Username :</label>
-          <input type="text" class="form-control" name = "Name" id="idName" placeholder="Enter Username"; ?> <br>
+          <input type="text" class="form-control" name = "Name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
           <label for="Username">Comment :</label>
-          <input type="text" class="form-control" name = "Comment" id="idComment" placeholder="Comment!!!"; ?> <br>
+          <input type="text" class="form-control" name = "Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
         <label for="Username">Link :</label>
-          <input type="text" class="form-control" name = "Link" id="idLink" placeholder="Enter Link"; ?> <br><br>
+          <input type="text" class="form-control" name = "Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
           <input type="submit" id="commentBtn"class="btn btn-outline-success">
     </div>
       </form>
